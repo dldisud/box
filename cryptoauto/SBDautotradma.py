9d876ec57ee7f05f0874eb6b60a2d8c64ea8a839
dldisud <87322286+dldisud@users.noreply.github.com>
2021-07-14 02:30:10 +0900

    Update SBDautotradma.py

--- cryptoauto/SBDautotradma.py
+++ cryptoauto/SBDautotradma.py
@@ -62,7 +62,7 @@ while True:
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-SBD", 0.8)
             ma15 = get_ma15("KRW-SBD")
-            current_price = get_current_price("KRW-BTC")
+            current_price = get_current_price("KRW-SBD")
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
